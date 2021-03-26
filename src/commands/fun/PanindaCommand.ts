c6cef1274d016542b8efc804eda1907179f4e343
potatoplex <twygstax@gmail.com>
2021-03-26 23:43:27 +0800

    cooldown

--- src/commands/fun/PanindaCommand.ts
+++ src/commands/fun/PanindaCommand.ts
@@ -3,10 +3,32 @@ import CommandGroup from '../../enums/CommandGroup';
 import Paninda from '../../models/Paninda';
 import DiscordApiService from '../../services/DiscordApiService';
 import { AsyncCommandRunType } from '../../typings';
-import { createEmbedMessage } from '../../util/MessageUtil';
-import { getRandomColor, pick } from '../../util/RngUtil';
+import { createEmbedMessage, mentionAuthor } from '../../util/MessageUtil';
+import { getRandomColor, pick, rateRoll } from '../../util/RngUtil';
+
+type RecentPanindaType = {
+	_id: string;
+	cooldown: number;
+	name: string;
+	userId: string;
+};
+
+const reklamos: (string | ((m: CommandoMessage) => string))[] = [
+	(m) =>
+		`${mentionAuthor(
+			m
+		)}, dami mo nang kinuha ha. Siguraduhin mong babayaran mo yan`,
+	(m) => `${mentionAuthor(m)}, wag kang mang hoard oi!`,
+	(m) => `Hinay hinay lang ${mentionAuthor(m)}! Pabilhin mo naman yung iba`,
+	(m) => `${mentionAuthor(m)}, ang dami mong binili because?`,
+	(m) =>
+		`${mentionAuthor(
+			m
+		)}, tama na yan nalulugi na tindahan ko sayo. Di ka nagbabayad e.`,
+];
 
 export default class PanindaCommand extends Command {
+	recent: RecentPanindaType[];
 	discordApiService: DiscordApiService;
 	constructor(client: CommandoClient) {
 		super(client, {
@@ -21,15 +43,51 @@ export default class PanindaCommand extends Command {
 			},
 		});
 		this.discordApiService = new DiscordApiService();
+		this.recent = [];
 	}
 
 	async run(message: CommandoMessage): AsyncCommandRunType {
-		const panindas = await Paninda.find({});
-		const paninda = pick(panindas.map(({ name }) => name));
+		this.recent = this.recent
+			.map(({ cooldown, ...r }) => ({ cooldown: +cooldown - 1, ...r }))
+			.filter(({ cooldown }) => cooldown > 0);
+
+		const count = await Paninda.count();
+		const cd = Math.floor(count * 0.4);
+
+		const panindas = await Paninda.find({
+			_id: { $nin: this.recent.map(({ _id }) => _id) },
+		});
+		const paninda = pick(panindas);
 		const embed = createEmbedMessage(getRandomColor()).setDescription(
-			paninda
+			paninda.name
 		);
 
-		return message.say(embed);
+		this.recent.push({
+			_id: paninda._id,
+			cooldown: cd,
+			name: paninda.name,
+			userId: message.author.id,
+		});
+
+		const purchaseCounter = this.recent.reduce(
+			(prev: { [key: string]: number }, next) => {
+				const prevCount = prev[next.userId] || 0;
+				return { ...prev, [next.userId]: +prevCount + 1 };
+			},
+			{}
+		);
+
+		const messages = [embed];
+
+		if (purchaseCounter[message.author.id] >= cd && rateRoll(40)) {
+			const reklamo = pick(reklamos);
+			messages.push(
+				createEmbedMessage(getRandomColor()).setDescription(
+					typeof reklamo === 'function' ? reklamo(message) : reklamo
+				)
+			);
+		}
+
+		return Promise.all(messages.map((m) => message.say(m)));
 	}
 }
